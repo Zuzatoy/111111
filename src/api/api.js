195bbed5ba6844348bac18651dc65635332e54de
Lisa Serous <liza@realestate.co.nz>
2020-07-09 11:28:09 +1200

    comment api route to make it works

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,6 @@ module.exports.handler = (evt, ctx, done) => {
     done(null, {
         statusCode: 200,
         headers: {},
-        body: JSON.stringify({message: 'yes baby wjik wjik'})
+        body: JSON.stringify({message: 'yes baby wjik wjik 11111'})
     })
 }
