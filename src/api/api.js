54216c0252e4bf869f473b5a834eae5e1685e65f
Lisa Serous <liza@realestate.co.nz>
2020-07-26 00:12:25 +1200

    add extra data and core

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,28 @@
-module.exports.handler = (evt, ctx, done) => {
-    done(null, {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify({message: 'yes baby wjik wjik 11111'})
-    })
-}
+
+const express = require('express')
+const app = express()
+var cors = require('cors')
+
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/todos', (req, res) => {
+    const todos = [
+        {id: 1, name: 'clean up', status: 'open'},
+        {id: 2, name: 'pook', status: 'done'},
+        {id: 3, name: 'kek', status: 'done'},
+        {id: 4, name: 'lick', status: 'done'},
+        {id: 5, name: 'shmik', status: 'done'}
+    ]
+    res.send({data: todos})
+})
+
+app.get('/todos/:todo', (req, res) => {
+    res.send({id: 1, name: 'clean up', status: 'open'})
+})
+
+module.exports = app;
